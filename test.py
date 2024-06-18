834dd528266ccdc15b872bdee20e1653916c094d
Nawaraj Bhujel <nawabro39@gmail.com>
2024-06-18 16:58:38 +0545

    hello

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import pytest
 from app import add  # replace 'app' with the name of your file without the .py
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(1, 2, 4) == 7
+    assert add(-1, 1, 5) == 5
+    assert add(0, 0, 6) == 6
